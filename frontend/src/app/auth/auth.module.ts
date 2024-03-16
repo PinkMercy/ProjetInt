1977f4fd833aa678c747d143dded6ae99c7c0061
rahmabacha <rahmabacha007@gmail.com>
2024-03-16 01:34:09 +0100

    starting

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -6,11 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {  HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
+
 import { CardModule } from 'primeng/card';
 
 
@@ -24,12 +20,7 @@ import { CardModule } from 'primeng/card';
     AuthRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatCardModule,
-  MatInputModule,
-  MatFormFieldModule,
-  MatCheckboxModule,
-  MatButtonModule,
-  CardModule
+    CardModule
   ]
 })
 export class AuthModule { }
