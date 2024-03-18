b99df79f8da0a89a00a9039760a7f0c888b51d6b
Ilef Neji <ilefneji334@gmail.com>
2024-03-18 22:06:42 +0100

    list-materiels

--- frontend/src/app/material/list-material/list-material.component.ts
+++ frontend/src/app/material/list-material/list-material.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Imaterial } from '../model/imaterial';
+import { MaterialModule } from '../material.module';
+import { MaterialService } from '../service/material.service';
 @Component({
   selector: 'app-list-material',
   templateUrl: './list-material.component.html',
   styleUrls: []
 })
 
-export class ListMaterialComponent {
-  
+export class ListMaterialComponent implements OnInit {
+  materials!:Imaterial[];
+  constructor(private service:MaterialService){}
+  ngOnInit(): void {
+      this.service.getAllMaterials().subscribe((m)=>this.materials=m);
+  }
   
 }
--- frontend/src/app/material/service/material.service.ts
+++ frontend/src/app/material/service/material.service.ts
@@ -2,27 +2,31 @@ import { Injectable } from '@angular/core';
 import { Imaterial } from '../model/imaterial';
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpClient} from '@angular/common/http';
+import { AuthService } from 'src/app/auth/service/auth.service';
+const BASE_URL = ["http://localhost:8080/"];
 @Injectable({
   providedIn: 'root'
 })
-const BASE_URL = ["http://localhost:8080/"];
+
 export class MaterialService {
+  headers= this.service.createAuhtorizationHeader()
+  
   getAllMaterials():Observable<Imaterial[]>{
-    return this.http.get<Imaterial[]>(BASE_URL+'api/material');
+    return this.http.get<Imaterial[]>(BASE_URL+'api/materiel',{headers:this.headers!});
   }
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private service:AuthService) { }
 
 addMaterial(a:Imaterial):Observable<Imaterial> {
-    return this.http.post<Imaterial>(BASE_URL + "api/material",a);
+    return this.http.post<Imaterial>(BASE_URL + "api/materiel",a,{headers:this.headers!});
   }
 
 updateMaterial(a:Imaterial):Observable<Imaterial> {
-    return this.http.put<Imaterial>(BASE_URL + "api/material/" + a.id,a);
+    return this.http.put<Imaterial>(BASE_URL + "api/materiel/" + a.id,a,{headers:this.headers!});
   }
 
 deleteMaterial(a:Imaterial):Observable<Imaterial> {
-    return this.http.delete<Imaterial>(BASE_URL + "api/material/" + a.id);
+    return this.http.delete<Imaterial>(BASE_URL + "api/materiel/" + a.id,{headers:this.headers!});
   }
   
 
